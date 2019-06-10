10ffcc460e41dc1d3ec79aa1232495c43b0e69e9
张维亚 <1010482327@qq.com>
2019-06-10 21:41:56 +0800

    优惠券数据管理中心

--- common/src/main/java/com/zhangwy/common/Common.java
+++ common/src/main/java/com/zhangwy/common/Common.java
@@ -1,5 +1,7 @@
 package com.zhangwy.common;
 
+import yixia.lib.core.sharePreferences.PreferencesHelper;
+
 /**
  * Created by zhangwy on 2019/5/20.
  * Updated by zhangwy on 2019/5/20.
@@ -7,4 +9,7 @@ package com.zhangwy.common;
  */
 public interface Common {
     String PFRC_SHOW_MEMBER_AVATAR = "pfrcShowMemberAvatar";//是否显示成员头像
+    String PFRC_NEAR_OVERDUE_DAY = "pfrcNearOverdueDay";//临近过期天数
+
+    long NEAR_OVERDUE_SM = PreferencesHelper.defaultInstance().getInt(PFRC_NEAR_OVERDUE_DAY, 3) * 24 * 3600000;
 }
